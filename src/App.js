d9232fb13978567bb833b4514e426ac5121d70aa
sing3demons <sing3demons@live.com>
2021-01-13 12:54:43 +0700

    hook useSate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react'
 
 export default function App() {
-  const initialState = {}
-  const [name, setname] = useState(initialState)
-  return <div></div>
+  const initialState = {
+    firstName: '',
+    lastName: '',
+  }
+  const [name, setName] = useState(initialState)
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => setName({ ...name, firstName: e.target.value })}
+      />
+      <br/>
+      <input
+        type="text"
+        onChange={(event) => setName({ ...name, lastName: event.target.value })}
+      />
+     
+      <div>{name.firstName}</div>
+      <div>{name.lastName}</div>
+    </>
+  )
 }
