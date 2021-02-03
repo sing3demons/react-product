1aa0309e3b22ddab9e9f6f39f15c4e4b37d548d7
sing3demons <sing3demons@live.com>
2021-02-03 18:37:29 +0700

    react-redux loadProduct

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from 'modules/reducer'
+import reduxThunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 export default function configureStore(initialState) {
-  const middleware = []
+  const middleware = [reduxThunk]
 
   const store = createStore(
     rootReducer,
